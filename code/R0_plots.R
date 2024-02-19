22c5f1b596027146c756db11e35fe4dba59f7bf6
Kyle Dahlin <kydahlin@gmail.com>
2024-02-19 17:29:31 -0500

    add R0 RV plot with higher base R0

--- code/R0_plots.R
+++ code/R0_plots.R
@@ -21,6 +21,8 @@
 library(tidyverse)
 library(cowplot)
 library(latex2exp)
+library(ggh4x)
+
 
 # 2) Define parameters and functions --------------------------------------
 # Baseline values for parameters depending on environmental noise:
@@ -38,43 +40,55 @@ muV_star = 0.1
 # gamma_H  - host recovery rate
 # N_H      - host population density
 NV = 100000
-tauHV = 0.03472222 # Gives R0 = 1.25 at baseline  
+tauHV_125 = 0.03472222
+tauHV_65 = tauHV_125 * 6.5 / 1.25
+tauHV_vals = c(tauHV_125, tauHV_65) # Gives R0 = 1.25 at baseline
 gammaH = 0.1
 NH = 10000
 
-C = sqrt(tauHV * NV / (gammaH * NH))
+# C = sqrt(tauHV * NV / (gammaH * NH))
 
-baseline_R0 = C * b_star * sqrt(tauVH_star / muV_star)
+# baseline_R0 = C * b_star * sqrt(tauVH_star / muV_star)
 
 # 3) Define axes of variation numerically ---------------------------------
 
 # Samples from Weiner process
-sample_res = 1000
-sample_vec = tibble(sample = rnorm(sample_res), ID = seq(1:sample_res))
+sample_res = 2 * 1000
+sample_vec1 = tibble(sample = rnorm(sample_res/2), ID = seq(1:(sample_res/2)))
+sample_vec2 = tibble(sample = rnorm(sample_res/2), ID = seq(1:(sample_res/2)))
 
 # sigma - strength of environmental noise
-sigma_res = 1000
-sigma_vec = seq(0, 0.4, length.out = sigma_res)
+sigma_res = 100
+sigma_vec = c(seq(0, 0.4, length.out = sigma_res), 0, 0.1, 0.2, 0.3, 0.4) %>% 
+  sort() %>% unique()
 
 eps = .Machine$double.eps
 
 # Put together data
-data_table <- cross_join(tibble(sigma = sigma_vec), sample_vec) %>% 
-  rowwise() %>% 
-  mutate(C = sqrt(tauHV * NV / (gammaH * NH)),
-         b = max(eps, b_star + sigma * sample), 
-         tauVH = max(eps, tauVH_star + sigma * sample), 
-         muV = max(1/60, muV_star + sigma * sample),
-         R0 = b * C * sqrt(tauVH / muV)
-  )
+data_table <- rbind(tibble(tauHV = tauHV_125, sample_vec1),
+                    tibble(tauHV = tauHV_65, sample_vec2)) %>% 
+  cross_join(tibble(sigma = sigma_vec)) %>%
+  rowwise() %>%
+  mutate(
+    R0 = max(eps, b_star + sigma * sample) * sqrt(tauHV * NV / (gammaH * NH)) * sqrt( max(eps, tauVH_star + sigma * sample) / (max(1/60, muV_star + sigma * sample)))
+    # C = sqrt(tauHV * NV / (gammaH * NH)),
+         # b = max(eps, b_star + sigma * sample), 
+         # tauVH = max(eps, tauVH_star + sigma * sample), 
+         # muV = max(1/60, muV_star + sigma * sample),
+         # R0 = b * C * sqrt(tauVH / muV)
+  ) %>% 
+  mutate(base_R0_label = case_when(
+    tauHV == tauHV_125 ~ 1.25,
+    tauHV == tauHV_65 ~ 6.5
+  ))
 
 summary_table <- data_table %>% 
-  group_by(sigma) %>% 
+  group_by(sigma, base_R0_label) %>% 
   summarise(mean_R0 = mean(R0),
             sd_R0 = sd(R0))
 
 condition_table <- data_table %>% 
-  group_by(sigma) %>% 
+  group_by(sigma, base_R0_label) %>% 
   summarise(prop_end = sum(R0 < 1) / n())
 
 # 4) Calculate Volume(R0 > 1) across axes ---------------------------------
@@ -95,9 +109,28 @@ summary_table %>%
                      expand = c(0,0)) +
   scale_y_continuous(unname(TeX("Basic reproduction number ($R_0$)")),
                      expand = c(0,0)) +
-  theme_cowplot()
+  facet_wrap(~ base_R0_label, scales = "free", ncol = 1,
+             labeller = as_labeller(function(string) {paste0("R0 = ", string)}))  +
+  theme_cowplot() +
+  theme(
+    strip.background = element_rect(color = NA, fill = NA)
+  )
 ggsave("./figures/R0vsnoise.png", height = 6, width = 8)
 
+# Discrete version
+data_table %>%
+  filter(sigma %in% c(0, 0.1, 0.2, 0.3, 0.4)) %>%
+  mutate(sigma = as.factor(sigma)) %>%
+  ggplot(aes(x = sigma, y = R0)) +
+  geom_violin() +
+  geom_line() +
+  geom_hline(yintercept = 1, color = "red") +
+  scale_x_discrete(unname(TeX("Environmental noise strength $(\\sigma)$")),
+                   expand = c(0,0)) +
+  scale_y_continuous(unname(TeX("Basic reproduction number ($R_0$)")),
+                     expand = c(0,0)) +
+  theme_cowplot()
+ggsave("./figures/violin_R0vsnoise.png", height = 6, width = 8)
 
 # Proportion of simulations with R0 > 1
 condition_table %>%
